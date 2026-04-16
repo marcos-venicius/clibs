62c9b6578543c2fbf606f09083b476f8874129e1
Marcos Venicius <marcosdevelopment.business@gmail.com>
2026-04-16 08:36:04 -0300

    feat(cearch): lex digits

--- cearch/src/lexer.c
+++ cearch/src/lexer.c
@@ -1,11 +1,16 @@
 #include "./lexer.h"
 #include "./location.h"
 
+#include <alloca.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define LOCATION_SNAPSHOTS_CAPACITY 256
+#define MAX_DIGIT_LENGTH 32
+#define MAX_FLOAT_POINT_LENGTH 32
 
 static Cearch_Location location_snapshots[LOCATION_SNAPSHOTS_CAPACITY] = {0};
 static int location_snapshots_size;
@@ -44,6 +49,8 @@ static void throw_error_message(Cearch_Location location, const char *fmt, ...) 
     fprintf(stderr, "\n");
 
     va_end(args);
+
+    exit(1);
 }
 
 const char *cearch_token_kind_name(Cearch_Token_Kind kind) {
@@ -75,10 +82,141 @@ const char *cearch_token_kind_name(Cearch_Token_Kind kind) {
     }
 }
 
-Cearch_Token *cearch_lex(Cearch_Lexer *lexer) {
-    save_location_snapshot(get_location_from_lexer(lexer));
+static inline char chr(const Cearch_Lexer *const lexer) {
+    return lexer->cursor < lexer->content_size ? lexer->content[lexer->cursor] : '\0';
+}
 
-    throw_error_message(get_location_snapshot(), "uninitialized lexer");
+static inline void advance_cursor(Cearch_Lexer *lexer) {
+    if (lexer->cursor < lexer->content_size) lexer->cursor++;
+    else if (lexer->content[lexer->cursor] == '\n') {
+        lexer->col = 1;
+        lexer->line++;
+    } else {
+        lexer->col++;
+    }
+}
+
+static inline void sync_bot(Cearch_Lexer *lexer) {
+    lexer->bot = lexer->cursor;
+}
+
+static inline bool is_empty(const Cearch_Lexer * const lexer) {
+    return chr(lexer) == '\0';
+}
+
+static inline bool is_digit(char c) {
+    return c >= '0' && c <= '9';
+}
+
+static inline void ltrim_whitespaces(Cearch_Lexer *lexer) {
+    while (!is_empty(lexer) && (chr(lexer) == '\n' || chr(lexer) == '\t' || chr(lexer) == ' ')) advance_cursor(lexer);
+}
+
+static inline void append_token(Cearch_Lexer *lexer, Cearch_Token *token) {
+    if (lexer->head == NULL) {
+        lexer->head = lexer->tail = token;
+    } else {
+        lexer->tail = lexer->tail->next = token;
+    }
+}
+
+static void lex_digit(Cearch_Lexer *lexer) {
+    while (is_digit(chr(lexer))) advance_cursor(lexer);
+
+    bool is_float = false;
+
+    int left_digit_length = lexer->cursor - lexer->bot;
+
+    if (chr(lexer) == '.') {
+        is_float = true;
+
+        advance_cursor(lexer);
+
+        while (is_digit(chr(lexer))) advance_cursor(lexer);
+    }
+
+    int digit_length = lexer->cursor - lexer->bot;
+
+    if (left_digit_length > MAX_DIGIT_LENGTH)
+        throw_error_message(get_location_snapshot(), "your digit ('%s') overflew the maximum length of %d digits", MAX_DIGIT_LENGTH);
+
+    if (digit_length - left_digit_length - 1 > MAX_FLOAT_POINT_LENGTH)
+        throw_error_message(get_location_snapshot(), "your digit ('%s') overflew the maximum float point length of %d digits", MAX_FLOAT_POINT_LENGTH);
+
+    char *digit = alloca(digit_length + 1);
+
+    memcpy(digit, lexer->content + lexer->bot, digit_length);
+
+    digit[digit_length] = '\0';
+
+    char *endptr;
+    Cearch_Token *token = malloc(sizeof(Cearch_Token));
+
+    if (is_float) {
+        double value = strtod(digit, &endptr);
+
+        if (digit == endptr) {
+            throw_error_message(get_location_snapshot(), "could not parse '%s' as float", digit);
+        }
+
+        *token = (Cearch_Token){
+            .content = (Cearch_String){
+                .value = lexer->content + lexer->bot,
+                .size = digit_length,
+            },
+            .kind = CT_FLOAT,
+            .as_float = value,
+            .location = get_location_snapshot(),
+            .next = NULL
+        };
+    } else {
+        long value = strtol(digit, &endptr, 10);
+
+        if (digit == endptr) {
+            throw_error_message(get_location_snapshot(), "could not parse '%s' as int", digit);
+        }
+
+        *token = (Cearch_Token){
+            .content = (Cearch_String){
+                .value = lexer->content + lexer->bot,
+                .size = digit_length,
+            },
+            .kind = CT_INT,
+            .as_int = value,
+            .location = get_location_snapshot(),
+            .next = NULL
+        };
+    }
+
+    append_token(lexer, token);
+}
+
+Cearch_Token *cearch_lex(Cearch_Lexer *lexer) {
+    ltrim_whitespaces(lexer);
+
+    while (!is_empty(lexer)) {
+        sync_bot(lexer);
+
+        save_location_snapshot(get_location_from_lexer(lexer));
+
+        switch (chr(lexer)) {
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                lex_digit(lexer);
+                break;
+            default:
+                throw_error_message(get_location_snapshot(), "unrecognized character '%c'", chr(lexer));
+                break;
+        }
+    }
 
     return lexer->head;
 }
--- cearch/src/test.c
+++ cearch/src/test.c
@@ -1,9 +1,41 @@
+#include <stdio.h>
+#include <string.h>
 #include "./lexer.h"
 
-int main(void) {
-    Cearch_Lexer lexer = {0};
+#define TEST_LEXER(q) do                                                        \
+{                                                                               \
+    printf("query: "q"\n");                                                     \
+    Cearch_Lexer lexer = { .content = q, .content_size = strlen(q) };           \
+    Cearch_Token *head = cearch_lex(&lexer);                                    \
+    if (head == NULL) {                                                         \
+        printf("  (null)\n");                                                   \
+    } else {                                                                    \
+        printf("  %s\n", cearch_token_kind_name(head->kind));                   \
+        switch (head->kind) {                                                   \
+            case CT_NIL: printf("  nil\n"); break;                              \
+            case CT_BOOL: printf(head->as_bool ? "  true" : "  false"); break;  \
+            case CT_INT: printf("  %d\n", head->as_int); break;                 \
+            case CT_FLOAT: printf("  %lf\n", head->as_float); break;            \
+            default:                                                            \
+                printf("  %.*s\n", head->content.size, head->content.value);    \
+                break;                                                          \
+        }                                                                       \
+    }                                                                           \
+    printf("\n");                                                               \
+} while(0);
 
-    cearch_lex(&lexer);
+int main(void) {
+    TEST_LEXER("1");
+    TEST_LEXER("1.");
+    TEST_LEXER("1.34");
+    TEST_LEXER("'Hello \\\\ world\\'s!\\t\\n'");
+    TEST_LEXER("true");
+    TEST_LEXER("false");
+    TEST_LEXER("nil");
+    TEST_LEXER("!= = > < >= <= !");
+    TEST_LEXER("and or");
+    TEST_LEXER("ltrim");
+    TEST_LEXER("");
 
     return 0;
 }
